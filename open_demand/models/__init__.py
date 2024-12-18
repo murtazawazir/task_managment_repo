2b41e33c710541fcc28c1e3480167895ccf2ba76
murtazawazir <murtazawaxir@gmail.com>
2024-12-18 18:42:06 +0500

    [ADD] new brach added

--- open_demand/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import open_demand
-from . import phone_validation
-from . import res_users
-from . import hr_recruitment_team
-
--- open_demand/models/res_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    recruitment_team_id = fields.Many2one(comodel_name='hr.recruitment.team', string='Users Recruitment Team', )
